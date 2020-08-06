955d57511ce4dad177a48b4f77d0d034731e8e3e
Pooja D <pooja.d@BLR.local>
2020-08-06 20:00:22 +0530

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ptolemaios',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/PoojaDurai97/ptolemaios-package',
+    author='Pooja Durai',
+    author_email='poojadurai1997@gmail.com',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)
